8abf150b29152d3384d61b0355a04ca9eb158fd7
Matthew Rice <mattricesound@gmail.com>
2025-03-06 16:07:46 -0800

    Try auth again 5

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main():
         model_half=args["model_half"]
     )
     interface.queue()
-    interface.launch(auth=("output", "becreative"))
+    interface.launch(debug = False, auth=(os.environ.get("USERNAME"), os.environ.get("PASSWORD")))
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser(description='Run gradio interface')
